a2978f682d8af26519abd2dffb04f57bc9d27f8d
Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>
2020-04-08 21:26:21 -0400

    Define iterable type yielding crev reviews
    Only a mockup of the eventual interface so far.

--- src/Crev.jl
+++ src/Crev.jl
@@ -1,5 +1,31 @@
 module Crev
 
-greet() = print("Hello World!")
+export ProofStream
+
+struct Proof end
+
+"""
+    ProofStream(io)
+
+Construct an iterator which yields crev proofs.
+"""
+struct ProofStream{T<:IO}
+    io::T
+end
+
+Base.IteratorSize(::Type{ProofStream}) = Base.SizeUnknown()
+Base.eltype(::Type{ProofStream}) = Proof
+
+function Base.iterate(ps::ProofStream, state=nothing)
+    readuntil(ps.io, "-----BEGIN CREV PACKAGE REVIEW-----")
+    yaml = readuntil(ps.io, "-----BEGIN CREV PACKAGE REVIEW SIGNATURE-----")
+    sig = readuntil(ps.io,  "-----END CREV PACKAGE REVIEW-----")
+
+    if isempty(yaml) || isempty(sig)
+        nothing
+    else
+        (Proof(), nothing)
+    end
+end
 
 end # module
