8e387b098355524eb56daa703698b90e6c932de7
Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>
2020-04-08 22:09:20 -0400

    Parse crev YAML
    Still missing some fields like "advisory" and "comment" and parsing is
    not verified by checking the signature

--- src/Crev.jl
+++ src/Crev.jl
@@ -2,7 +2,70 @@ module Crev
 
 export ProofStream
 
-struct Proof end
+import YAML
+
+const M{T} = Union{Nothing,T}
+
+struct CrevID
+    id::String
+    url::String
+end
+
+struct Package
+    source::String
+    name::String
+    version::VersionNumber
+    revision::M{String}
+    digest::String
+end
+
+# TODO: use some type of enum for the fields instead?
+struct Review
+    thoroughness::String
+    understanding::String
+    rating::String
+end
+
+struct Proof
+    version::Int
+    # TODO: parse this into a TimeZones.jl ZonedDateTime
+    date::String
+    id::CrevID
+    package::Package
+    review::Review
+end
+
+struct MalformedProof end
+
+function Proof(yaml::AbstractString)
+    try
+        parsed_yaml = YAML.load(yaml)
+
+        # Ensure id-type is crev
+        @assert parsed_yaml["from"]["id-type"] == "crev"
+        id = CrevID(parsed_yaml["from"]["id"], parsed_yaml["from"]["url"])
+
+        package = Package(
+            parsed_yaml["package"]["source"],
+            parsed_yaml["package"]["name"],
+            VersionNumber(parsed_yaml["package"]["version"]),
+            get(parsed_yaml["package"], "revision", nothing),
+            parsed_yaml["package"]["digest"])
+
+        review = Review(
+            parsed_yaml["review"]["thoroughness"],
+            parsed_yaml["review"]["understanding"],
+            parsed_yaml["review"]["rating"])
+
+        Proof(parsed_yaml["version"], parsed_yaml["date"], id, package, review)
+    catch e
+        if e isa YAML.ParserError || e isa KeyError || e isa AssertionError
+            return MalformedProof()
+        else
+            rethrow()
+        end
+    end
+end
 
 """
     ProofStream(io)
@@ -14,7 +77,7 @@ struct ProofStream{T<:IO}
 end
 
 Base.IteratorSize(::Type{ProofStream}) = Base.SizeUnknown()
-Base.eltype(::Type{ProofStream}) = Proof
+Base.eltype(::Type{ProofStream}) = Union{Proof,MalformedProof}
 
 function Base.iterate(ps::ProofStream, state=nothing)
     readuntil(ps.io, "-----BEGIN CREV PACKAGE REVIEW-----")
@@ -24,7 +87,7 @@ function Base.iterate(ps::ProofStream, state=nothing)
     if isempty(yaml) || isempty(sig)
         nothing
     else
-        (Proof(), nothing)
+        (Proof(yaml), nothing)
     end
 end
 
